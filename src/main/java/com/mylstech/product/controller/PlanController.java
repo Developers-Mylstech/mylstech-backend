885e3e965cb9bd737d1e4d2a57bdbeb458fb018e
uzair <uzairuddin7470@gmail.com>
2025-04-16 18:36:46 +0530

    add update plan api

--- src/main/java/com/mylstech/product/controller/PlanController.java
+++ src/main/java/com/mylstech/product/controller/PlanController.java
@@ -14,17 +14,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/plans")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PlanController {
     private final PlanService planService;
-
     @PostMapping("/addPlan")
     public ResponseEntity<PlanResponse> addPlan(@RequestBody PlanRequest request) {
        return new ResponseEntity<> (  planService.addPlan(request), HttpStatus.OK);
-
     }
     @GetMapping("/getPlan")
     public ResponseEntity<List<PlanResponse>> getPlans() {
         return new ResponseEntity<> ( planService.getPlan(), HttpStatus.OK );
+    }
+    @PutMapping("/updatePlan/{planId}")
+    public ResponseEntity<PlanResponse> updatePlan(@PathVariable Long planId, @RequestBody PlanRequest request) {
+        return new ResponseEntity<> ( planService.updatePlan(planId,request), HttpStatus.OK );
     }
 }
