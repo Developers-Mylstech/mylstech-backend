5a61fc409c37428a3896940280575d1121d74a7b
uzair <uzairuddin7470@gmail.com>
2025-04-16 18:37:59 +0530

    change planType from string to PlanType enum

--- src/main/java/com/mylstech/product/dto/request/PlanRequest.java
+++ src/main/java/com/mylstech/product/dto/request/PlanRequest.java
@@ -1,9 +1,12 @@
 package com.mylstech.product.dto.request;
 
 import com.mylstech.product.model.Plan;
+import com.mylstech.product.util.PlanType;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class PlanRequest {
@@ -13,17 +16,24 @@ public class PlanRequest {
     private Double pricing;
     private Boolean status;
     private Integer duration;
-    private String planType;
+    private PlanType planType;
+    private List<String> highlights = new ArrayList<> ( );
 
     public Plan toPlan(PlanRequest request) {
         Plan plan = new Plan ( );
-        plan.setImageUrl ( request.imageUrl );
-        plan.setTitle ( request.title );
-        plan.setDescription ( request.description );
-        plan.setPricing ( BigDecimal.valueOf ( request.pricing ) );
-        plan.setStatus ( request.status );
-        plan.setDuration ( request.duration );
-        plan.setPlanType ( request.planType );
+        plan.setImageUrl ( request.imageUrl != null ? request.imageUrl : "" );
+        plan.setTitle ( request.title != null ? request.title : "title" );
+        plan.setDescription ( request.description != null ? request.description : "" );
+        plan.setPricing ( request.pricing != null ? BigDecimal.valueOf ( request.pricing ) : BigDecimal.ZERO );
+        plan.setStatus ( request.status != null ? request.status : false );
+        plan.setDuration ( request.duration != null ? request.duration : 0 );
+        plan.setPlanType ( request.planType != null ? request.planType : PlanType.MONTHLY );
+        if (this.highlights != null && !this.highlights.isEmpty()) {
+            if (plan.getHighlightsEmbedded() == null) {
+                plan.setHighlightsEmbedded(new ArrayList<>());
+            }
+            plan.getHighlightsEmbedded().addAll(this.highlights);
+        }
         return plan;
     }
 
