519a2152f84f80b13463be375eff22c6cc0704b6
uzair <uzairuddin7470@gmail.com>
2025-04-16 18:39:58 +0530

    add update  plan api

--- src/main/java/com/mylstech/product/impl/PlanServiceImpl.java
+++ src/main/java/com/mylstech/product/impl/PlanServiceImpl.java
@@ -8,6 +8,7 @@ import com.mylstech.product.service.PlanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -24,6 +25,39 @@ public class PlanServiceImpl implements PlanService {
     @Override
     public List<PlanResponse> getPlan() {
         return planRepository.findAll ().stream ().map ( PlanResponse::new ).toList ();
+    }
+
+    @Override
+    public PlanResponse updatePlan(Long planId, PlanRequest request) {
+        Plan existingPlan = planRepository.findById ( planId ).orElseThrow ( () -> new RuntimeException ( "plan not found" ) );
+        if(request.getTitle () != null) {
+            existingPlan.setTitle ( request.getTitle () );
+        }
+        if (request.getDescription() != null) {
+            existingPlan.setDescription(request.getDescription());
+        }
+        if(request.getImageUrl () != null) {
+            existingPlan.setImageUrl ( request.getImageUrl () );
+        }
+        if (request.getPricing() != null) {
+            existingPlan.setPricing( BigDecimal.valueOf(request.getPricing()));
+        }
+        if (request.getStatus() != null) {
+            existingPlan.setStatus(request.getStatus());
+        }
+        if (request.getDuration() != null) {
+            existingPlan.setDuration(request.getDuration());
+        }
+        if (request.getPlanType() != null) {
+            existingPlan.setPlanType(request.getPlanType());
+        }
+        if (request.getHighlights() != null) {
+            existingPlan.getHighlightsEmbedded().addAll(request.getHighlights());
+
+        }
+
+        Plan updatedPlan = planRepository.save(existingPlan);
+        return new PlanResponse(updatedPlan);
     }
 
 
