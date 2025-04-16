d020d194db0f43740935ea174f2c2adceee12f21
uzair <uzairuddin7470@gmail.com>
2025-04-16 18:29:33 +0530

    add many to many relation between cart and plan

--- src/main/java/com/mylstech/product/controller/CartController.java
+++ src/main/java/com/mylstech/product/controller/CartController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/cart")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CartController {
     private final CartService cartService;
 
