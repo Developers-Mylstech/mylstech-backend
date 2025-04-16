1e735cc51eda2a1730d673f95cac8eba6d98cbbb
uzair <uzairuddin7470@gmail.com>
2025-04-16 18:33:50 +0530

    add cors configuration with mylstech domain

--- src/main/java/com/mylstech/product/model/Highlight.java
+++ src/main/java/com/mylstech/product/model/Highlight.java
@@ -1,26 +1,19 @@
 package com.mylstech.product.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.mylstech.product.dto.request.HighlightRequest;
+import jakarta.persistence.Embeddable;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Embeddable
 @Data
-@Entity
-@Table(name = "highlights")
-@EqualsAndHashCode(of = "highlightId")
+@NoArgsConstructor
 public class Highlight {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long highlightId;
-    
-    @Column(nullable = false, length = 100)
     private String title;
-    
-    @Column(length = 500)
     private String description;
+
+    public Highlight(HighlightRequest request) {
+        this.title =request.getTitle();
+        this.description =request.getDescription();
+    }
 }
